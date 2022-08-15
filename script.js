d2e6b966cdd42eee2ce385cf4da44fdae14917ef
louiseweb <36304256+louiseweb@users.noreply.github.com>
2022-08-15 17:03:48 +0100

    Mobile nav section started

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
-const openBtn = document.querySelector('#btn-open');
-const closeBtn = document.querySelector('#btn-close');
+const overlay = document.querySelector("#overlay");
+const openBtn = document.querySelector("#btn-open");
+const closeBtn = document.querySelector("#btn-close");
+const nav = document.querySelector("#nav");
 
-
-
-openBtn.addEventListener('click', function(){
-    openBtn.classList.add('d-none');
-    closeBtn.classList.remove('d-none');
+openBtn.addEventListener("click", function () {
+  openBtn.classList.add("d-none");
+  closeBtn.classList.remove("d-none");
+  nav.classList.remove("d-none");
+  overlay.classList.remove("d-none");
 });
 
-closeBtn.addEventListener('click', function(){
-    closeBtn.classList.add('d-none');
-    openBtn.classList.remove('d-none');
+closeBtn.addEventListener("click", function () {
+  closeBtn.classList.add("d-none");
+  openBtn.classList.remove("d-none");
+  nav.classList.add("d-none");
+  overlay.classList.add("d-none");
 });
